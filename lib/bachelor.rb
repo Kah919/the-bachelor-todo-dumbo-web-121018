bd0d1a21393c0d4d834a6436fc3b09d242169d96
Kah Yap <kahyap13@gmail.com>
2018-11-27 09:18:16 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,21 +1,27 @@
 def get_first_name_of_season_winner(data, season)
   data[season].each do |person|
-    if person["status"] == "Winner"
+    if person["status"] == "Winner" # reason why it didn't work for me before is because i used symbol ":" instead of ""
       return person["name"].split.first
     end
   end
-
   # data[season.to_sym].each do |person| #person is now the only ones from season
   #   person.each do |key, value| #diff keys and their values
   #     if person[key] == "Winner"
   #       return person[:name].split.first
   #     end
   #   end
-  # end
+  # end # this works too buy only on repl
 end
 
 def get_contestant_name(data, occupation)
   # code here
+  data.each do |season, arr|
+    arr.each do |job|
+      if job[:occupation] == occupation
+        return job[:name]
+      end
+    end
+  end
 end
 
 def count_contestants_by_hometown(data, hometown)
