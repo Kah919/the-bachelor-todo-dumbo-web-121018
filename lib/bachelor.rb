657aaae6a544e55e2b87014452e9cf6b2dc146dc
Kah Yap <kahyap13@gmail.com>
2018-11-27 07:52:16 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -1,9 +1,18 @@
 def get_first_name_of_season_winner(data, season)
-  data[season].each do |contestant|
-    if contestant["status"] == "Winner"
-      return contestant["name"].split(" ")[0]
+  def get_first_name_of_season_winner(data, season)
+  data[season.to_sym].each do |person|
+    if person[:status] == "Winner"
+      return person[:name].split.first
     end
   end
+end
+  # data[season.to_sym].each do |person| #person is now the only ones from season
+  #   person.each do |key, value| #diff keys and their values
+  #     if person[key] == "Winner"
+  #       return person[:name].split.first
+  #     end
+  #   end
+  # end
 end
 
 def get_contestant_name(data, occupation)
